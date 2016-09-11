442bfec802b531394b9c93717ce0ee62500a5536
livSmitty <livia@jansenresearch.com>
2016-09-11 16:15:57 -0700

    Commit to Git

---
+++ src/NFLPlayer.java
@@ -0,0 +1,14 @@
+/**
+ * Created by scott_000 on 9/11/2016.
+ */
+public class NFLPlayer {
+    String name;
+    double passingYards;
+    double rushingYards;
+    double receivingYards;
+    int tackles;
+    int sacks;
+    int interceptions;
+
+
+}
